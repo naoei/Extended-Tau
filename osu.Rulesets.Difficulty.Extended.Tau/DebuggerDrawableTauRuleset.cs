de10268487f2411c7b4075050ebb28a0f6b6f2c6
naoei <altenyt@gmail.com>
2022-05-20 12:08:13 -0400

    Create debugger ruleset

---
+++ osu.Rulesets.Difficulty.Extended.Tau/DebuggerDrawableTauRuleset.cs
@@ -0,0 +1,68 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Tau;
+using osu.Game.Rulesets.Tau.UI;
+using osu.Game.Rulesets.UI;
+using osuTK;
+
+namespace osu.Rulesets.Difficulty.Extended.Tau;
+
+public class DebuggerDrawableTauRuleset : TauDrawableRuleset
+{
+    public DebuggerDrawableTauRuleset(TauRuleset ruleset, IBeatmap beatmap, IReadOnlyList<Mod> mods = null)
+        : base(ruleset, beatmap, mods)
+    {
+    }
+
+    protected override Playfield CreatePlayfield() => new TauDebuggerPlayfield();
+
+    public override PlayfieldAdjustmentContainer CreatePlayfieldAdjustmentContainer()
+        => new TauDebuggerPlayfieldAdjustmentContainer(0.9f);
+
+    private class TauDebuggerPlayfield : TauPlayfield
+    {
+        protected override GameplayCursorContainer CreateCursor() => new InvisibleTauCursor();
+
+        private class InvisibleTauCursor : TauCursor
+        {
+            public InvisibleTauCursor()
+            {
+                Alpha = 0;
+                AlwaysPresent = true;
+            }
+        }
+    }
+    
+    private class TauDebuggerPlayfieldAdjustmentContainer : PlayfieldAdjustmentContainer
+    {
+        protected override Container<Drawable> Content => content;
+        private readonly Container content;
+
+        public TauDebuggerPlayfieldAdjustmentContainer(float scale = 0.6f)
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+            Size = new Vector2(scale);
+            FillMode = FillMode.Fit;
+            FillAspectRatio = 1;
+
+            InternalChild = content = new ScalingContainer { RelativeSizeAxes = Axes.Both };
+        }
+
+        /// <summary>
+        /// A <see cref="Container"/> which scales its content relative to a target width.
+        /// </summary>
+        private class ScalingContainer : Container
+        {
+            protected override void Update()
+            {
+                base.Update();
+
+                Scale = new Vector2(Parent.ChildSize.X / TauPlayfield.BaseSize.X);
+                Size = Vector2.Divide(Vector2.One, Scale);
+            }
+        }
+    }
+}
--- osu.Rulesets.Difficulty.Extended.Tau/ExtendedTauRuleset.cs
+++ osu.Rulesets.Difficulty.Extended.Tau/ExtendedTauRuleset.cs
@@ -1,6 +1,8 @@
 ﻿using osu.Framework.IO.Stores;
 using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Tau;
+using osu.Game.Rulesets.UI;
 using osu.Rulesets.Difficulty.Extended.Tau.Difficulty;
 using osu.Rulesets.Difficulty.Extended.Tau.Performance;
 using osu.Rulesets.Difficulty.Statistics.Rulesets;
@@ -16,6 +18,9 @@ public class ExtendedTauRuleset : TauRuleset, IExtendedRuleset
 
     public IExtendedPerformanceCalculator CreateExtendedPerformanceCalculator()
         => new ExtendedTauPerformanceCalculator();
+
+    public DrawableRuleset CreateDebuggerRuleset(IBeatmap beatmap, IReadOnlyList<Mod> mods = null)
+        => new DebuggerDrawableTauRuleset(this, beatmap, mods);
 
     public override IResourceStore<byte[]> CreateResourceStore() => new NamespacedResourceStore<byte[]>(new DllResourceStore(typeof(TauRuleset).Assembly), @"Resources");
 }
